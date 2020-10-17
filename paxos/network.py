0c6f3cfd4d7add2e0f74e50da78120e8c5ff5e07
seandi <eandi.simo@outlook.it>
2020-10-17 11:25:58 +0200

    add static methods to create sender and receiver multicast sockets

--- paxos/network.py
+++ paxos/network.py
@@ -1,12 +1,15 @@
 from typing import Tuple, NewType
 from paxos.role import Role
 from copy import deepcopy
-
+import socket
+import struct
 
 NetworkGroup = NewType('NetworkGroup', Tuple[str, int])
 
 
 class Network:
+    SOCKET_BUFFSIZE = 2**16
+
     def __init__(self,
                  size: int,
                  clients: NetworkGroup,
@@ -32,3 +35,28 @@ class Network:
         Gets a network group data by role
         """
         return deepcopy(self.__dict[role])
+
+    @staticmethod
+    def udp_sender_socket() -> socket.SocketType:
+        """
+        Create UDP socket
+        """
+        return socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+    @staticmethod
+    def multicast_receiver_socket(socket_address: NetworkGroup) -> socket.SocketType:
+        """
+        Create multicast receiver socket
+        """
+        mcast_receiver_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        mcast_receiver_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        mcast_receiver_sock.bind(socket_address)
+
+        # create multicast group and add the receiver socket to it
+        mcast_group_ip = socket_address[0]
+        mcast_group = socket.inet_aton(mcast_group_ip)
+        mcast_receiver_sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mcast_group)
+
+        return mcast_receiver_sock
+
+
