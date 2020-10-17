135835e340fc2722db0c78a93fcc92a64eededa8
seandi <eandi.simo@outlook.it>
2020-10-17 13:08:12 +0200

    implement fake proposer to periodically broadcast message and fake acceptor to echo received messages

--- paxos/acceptor.py
+++ paxos/acceptor.py
@@ -2,6 +2,7 @@ from typing import NoReturn
 from .role import Role
 from .network import Network
 from .node import NodeID, Node
+from .node import NodeID, Node, Message, SeqID, PaxosID, Payload
 
 
 class Acceptor(Node):
@@ -11,4 +12,10 @@ class Acceptor(Node):
     def run(self) -> NoReturn:
         print(f'Entering paxos in a role of acceptor {self.id}')
         while True:
-            pass
+            message: Message = self.listen()
+            msg_sender = message.sender_id
+            msg_round = message.payload[0]
+            msg_text = message.payload[2]
+            print("Received message from acceptor {0} for round {1} with text: {2} "
+                  .format(msg_sender, msg_round, msg_text)
+                  )
--- paxos/proposer.py
+++ paxos/proposer.py
@@ -1,8 +1,8 @@
 from typing import NoReturn
 from .role import Role
 from .network import Network
-from .node import NodeID, Node
-
+from .node import NodeID, Node, Message, SeqID, PaxosID, Payload
+import time
 
 class Proposer(Node):
     def __init__(self, id: NodeID, network: Network) -> None:
@@ -10,5 +10,13 @@ class Proposer(Node):
 
     def run(self) -> NoReturn:
         print(f'Entering paxos in a role of proposer {self.id}')
+        round = 0
         while True:
-            pass
+            round += 1
+            message = Message(sender=self,
+                              receiver_role=Role.ACCEPTOR,
+                              payload=Payload( (SeqID(round), PaxosID(round), "Hello!"))
+                              )
+            self.send(0, message)
+            print("Sent message for round {}".format(round))
+            time.sleep(5)
