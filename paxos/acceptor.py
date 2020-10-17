9445116aea229cc28087b94e6f1e25bb95dfe84c
seandi <eandi.simo@outlook.it>
2020-10-17 19:50:07 +0200

    implement phases 1A(prepare) and 1B (promise)

--- paxos/acceptor.py
+++ paxos/acceptor.py
@@ -2,20 +2,44 @@ from typing import NoReturn
 from .role import Role
 from .network import Network
 from .node import NodeID, Node
-from .node import NodeID, Node, Message, SeqID, PaxosID, Payload
+from .message import RoundID, PaxosValue
+from .message import PreparePayload, Prepare, PromisePayload, Promise
 
 
 class Acceptor(Node):
     def __init__(self, id: NodeID, network: Network) -> None:
         super().__init__(id, Role.ACCEPTOR, network)
+        # Latest round the acceptor has participated in
+        self.latest_round_ID: RoundID = RoundID(0)
+        # Accepted value, if any
+        self.accepted_value: PaxosValue = PaxosValue(0)
+        # Round in which the accepted value was accepted
+        self.accepted_round_ID: RoundID = RoundID(0)
+        self.accepted_value_yet: bool = False
+
+    def promise(self):
+        prepare_message: Prepare = self.listen()
+        payload: PreparePayload = prepare_message.payload
+        round_ID: RoundID = payload[0]
+        if round_ID > self.latest_round_ID:
+            self.latest_round_ID = round_ID
+            promise_message: Promise = Promise(sender=self,
+                                               receiver_role=Role.PROPOSER,
+                                               payload=PromisePayload((self.latest_round_ID,
+                                                                       self.accepted_round_ID,
+                                                                       self.accepted_value))
+                                               )
+            self.send(group=Role.PROPOSER, message=promise_message)
+            print("Sending Promise for round ID: {}".format(self.latest_round_ID))
+
+    def accept(self):
+        pass
+
 
     def run(self) -> NoReturn:
         print(f'Entering paxos in a role of acceptor {self.id}')
+
         while True:
-            message: Message = self.listen()
-            msg_sender = message.sender_id
-            msg_round = message.payload[0]
-            msg_text = message.payload[2]
-            print("Received message from acceptor {0} for round {1} with text: {2} "
-                  .format(msg_sender, msg_round, msg_text)
-                  )
+            self.promise()
+            self.accept()
+
--- paxos/proposer.py
+++ paxos/proposer.py
@@ -1,22 +1,38 @@
 from typing import NoReturn
 from .role import Role
 from .network import Network
-from .node import NodeID, Node, Message, SeqID, PaxosID, Payload
+from .node import NodeID, Node
+from .message import RoundID, PaxosValue
+from .message import PreparePayload, Prepare, Propose
 import time
+
+# Naive solution to guarantee uniqueness of round ID between multiple proposers
+primes = [2,3,5,7,11,13,17]
 
 class Proposer(Node):
     def __init__(self, id: NodeID, network: Network) -> None:
         super().__init__(id, Role.PROPOSER, network)
+        # The ID of the round currently initiated by the proposer
+        self._round_id: RoundID = RoundID(1)
+        # The value to be proposed in the next PROPOSE phase
+        self._propose_next_value = PaxosValue(0)
+        # The value that the proposer will try to propose if unconstrained
+        self._desired_value = self.id
+
+    def prepare_phase(self) -> None:
+        self._round_id *= primes[self.id]
+        prepare_message = Prepare(sender=self,
+                                  receiver_role=Role.ACCEPTOR,
+                                  payload=PreparePayload((self._round_id,))
+                                  )
+        self.send(Role.ACCEPTOR, prepare_message)
+
+    def propose_phase(self) -> None:
+        time.sleep(10)
 
     def run(self) -> NoReturn:
         print(f'Entering paxos in a role of proposer {self.id}')
-        round = 0
+
         while True:
-            round += 1
-            message = Message(sender=self,
-                              receiver_role=Role.ACCEPTOR,
-                              payload=Payload( (SeqID(round), PaxosID(round), "Hello!"))
-                              )
-            self.send(0, message)
-            print("Sent message for round {}".format(round))
-            time.sleep(5)
+            self.prepare_phase()
+            self.propose_phase()
