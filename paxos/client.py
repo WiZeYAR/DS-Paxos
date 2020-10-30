b3372d0c8b792fa5e9b1e7c59c3e179ab4fc1c25
seandi <eandi.simo@outlook.it>
2020-10-30 12:22:52 +0100

    change clients to retry requesting value until ACK is received

--- paxos/client.py
+++ paxos/client.py
@@ -1,17 +1,26 @@
 from typing import NoReturn
 from .role import Role
 from .network import Network
-from .node import NodeID, Node
-from .message import PaxosValue, InstanceID, ClientPropose, ClientProposePayload
-from numpy import random
-from time import sleep
+from .node import NodeID, Node, MessageT
+from .message import PaxosValue, InstanceID, ClientPropose, ClientProposePayload, MessageType
+
+import time
 import sys
+from typing import Dict
 
 
 class Client(Node):
+    BASE_TIMEOUT = 0.33
+    TIMEOUT_GROWTH_FACTOR = 1.1
+
     def __init__(self, id: NodeID, network: Network) -> None:
         super().__init__(id, Role.CLIENT, network)
         self._instance_id: InstanceID = InstanceID(0)
+        self._pending_requests: Dict[InstanceID, ClientPropose] = {}
+
+        # Timeout for receiving an ACK for each instance value request
+        self._request_timeouts: Dict[InstanceID, float] = {}
+        self._last_request_time: Dict[InstanceID, float] = {}
 
     def request_value(self, value: int):
         """Send a request to all proposers to propose a given value"""
@@ -23,9 +32,32 @@ class Client(Node):
                                                        )
                                                        )
         self.send(request_message)
+        self._pending_requests[self._instance_id] = request_message
+        self._last_request_time[self._instance_id] = time.time()
+
 
     def run(self) -> NoReturn:
         self.log("Start running...")
+
+        for value in sys.stdin:
+            self.request_value(value.strip())
+            self._request_timeouts[self._instance_id] = Client.BASE_TIMEOUT
+
         while True:
-            for value in sys.stdin:
-                self.request_value(value.strip())
+            # If an ACK is received remove request for the corresponding instance from the set of pending request
+            message: MessageT = self.listen()
+            if message is not None and message.message_type is MessageType.REQUEST_ACK:
+                instance: InstanceID = message.payload
+                if instance in self._pending_requests:
+                    del self._pending_requests[instance]
+
+            # Check if a value request timed out
+            for instance in self._pending_requests.keys():
+                if (time.time() - self._last_request_time[instance]) > self._request_timeouts[instance]:
+                    # Increase timeout and try resend the request
+                    self._request_timeouts[instance] *= Client.TIMEOUT_GROWTH_FACTOR
+                    self.send(self._pending_requests[instance])
+
+
+
+
