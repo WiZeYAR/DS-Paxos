d413e9af3c654442d24870b67bb4ed254b5011f8
seandi <eandi.simo@outlook.it>
2020-11-01 15:47:16 +0100

    update run script

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ conf=./paxos/paxos.conf
 n="$2"
 plr="$3"
 resultdir=./results
+time="$4"
+no_timeouts="$5"
+no_prexecution="$6"
 
 if [[ x$projdir == "x" || x$n == "x" || x$plr == "x" ]]; then
 	echo "Usage: $0 <project dir> <number of values per proposer> <plr>"
@@ -27,37 +30,37 @@ rm -r $resultdir/*
 
 echo "starting acceptors..."
 
-./acceptor.sh 1 $conf $plr &
-./acceptor.sh 2 $conf $plr &
-./acceptor.sh 3 $conf $plr &
+./acceptor.sh 1 $conf $plr $time &
+./acceptor.sh 2 $conf $plr $time &
+./acceptor.sh 3 $conf $plr $time &
 
 sleep 1
 echo "starting learners..."
 
-./learner.sh 1 $conf  $plr &
-./learner.sh 2 $conf  $plr &
+./learner.sh 1 $conf  $plr $time &
+./learner.sh 2 $conf  $plr $time &
+
 
 
 sleep 1
 echo "starting proposers..."
 
-./proposer.sh 1 $conf $plr &
-./proposer.sh 2 $conf $plr &
-./proposer.sh 3 $conf $plr &
-#./proposer.sh 4 $conf $plr &
-#./proposer.sh 5 $conf $plr &
-#./proposer.sh 6 $conf $plr &
+./proposer.sh 1 $conf $plr $time $no_timeouts $no_prexecution &
+./proposer.sh 2 $conf $plr $time &
+./proposer.sh 3 $conf $plr $time &
+./proposer.sh 4 $conf $plr $time &
 
 
 echo "waiting to start clients"
 sleep 3
 echo "starting clients..."
 
-./client.sh 1 $conf $plr < "$resultdir"/propose1.txt &
-./client.sh 2 $conf $plr < "$resultdir"/propose2.txt &
+./client.sh 1 $conf $plr $time < "$resultdir"/propose1.txt &
+./client.sh 2 $conf $plr $time < "$resultdir"/propose2.txt &
 
+sleep $time
+sleep 5
 
-sleep 30
 
 $KILLCMD
 wait
