f34c2c1682e020a0ccab82afd646ccaeee793f5e
Yaroslav V. Petrov <yaroslav.v.petrov@yandex.ru>
2020-11-02 05:01:02 +0100

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Executing docker-entrypoint.sh..."
+echo "Current role is $ROLE"
+echo "Current id is $ID"
+echo "Running script ./$ROLE.sh"
+echo "Exited from docker-entrypoint.sh."
